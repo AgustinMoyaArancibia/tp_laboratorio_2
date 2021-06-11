d024eedc770ab0593467357e8d9f7804b5cdfebf
agustin moya <agustin.moya.4219@gmail.com>
2021-06-11 15:37:46 -0300

    11-6

--- tp3 laboratorio2/FrmPrincipal/FrmCrearCartas.cs
+++ tp3 laboratorio2/FrmPrincipal/FrmCrearCartas.cs
@@ -12,6 +12,7 @@ using System.IO;
 using System.Xml;
 using System.Xml.Serialization;
 using Archivos;
+using Excepciones;
 
 namespace FrmPrincipal
 {
@@ -50,34 +51,33 @@ namespace FrmPrincipal
         {
             Carta carta = gdvCartasPorCrear.CurrentRow.DataBoundItem as Carta; // obtengo la fila seleccionada y la convierto en carta
 
-          
 
-            try
+
+
+            if (carta.Madera <= FabricaPegasus.Madera && carta.PorcentajeTinta <= FabricaPegasus.PorcentajeTinta && carta.Carton <= FabricaPegasus.Carton)
             {
-                if (carta.Madera <= FabricaPegasus.Madera && carta.PorcentajeTinta <= FabricaPegasus.PorcentajeTinta && carta.Carton <= FabricaPegasus.Carton)
-                {
 
 
 
-                    FabricaPegasus.Madera -= carta.Madera;
-                    FabricaPegasus.PorcentajeTinta -= carta.PorcentajeTinta;
-                    FabricaPegasus.Carton -= carta.Carton;
+                FabricaPegasus.Madera -= carta.Madera;
+                FabricaPegasus.PorcentajeTinta -= carta.PorcentajeTinta;
+                FabricaPegasus.Carton -= carta.Carton;
 
-                    carta.Stock++;
-                    //foreach (Carta item in cartasDisponibles)
-                    //{
-                    //    item.Stock += 1;
-                    //}
-                }
+                carta.Stock++;
+                //foreach (Carta item in cartasDisponibles)
+                //{
+                //    item.Stock += 1;
+                //}
+            }
+            else 
+            {
+                MessageBox.Show("compre mas materiales para seguir reponiendo stock");
+            }
 
                 rtbInfoMateriales.Text = FabricaPegasus.MostrarMaterialesFabrica();
                 this.gdvCartasPorCrear.DataSource = cartasDisponibles; //se lo paso al datagreed
-            }
-            catch (Exception)
-            {
-
-                throw;
-            }
+                
+           
         }
 
         private void button3_Click(object sender, EventArgs e)
