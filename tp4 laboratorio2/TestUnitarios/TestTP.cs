91bcfc564f1eaa2d0f967eea731776929d62665a
agustin moya <agustin.moya.4219@gmail.com>
2021-07-19 01:15:50 -0300

    se arreglo el test de guardar archivo , se testeo que no halla ninguna excepcion no controlada

--- tp4 laboratorio2/TestUnitarios/TestTP.cs
+++ tp4 laboratorio2/TestUnitarios/TestTP.cs
@@ -12,22 +12,22 @@ namespace TestUnitarios
         /// <summary>
         /// analiza si se agergo una carta a la lista 
         /// </summary>
-        [TestMethod] 
+        [TestMethod]
         public void AgregaLista()
         {
 
-           
+
 
             List<Carta> cartas = new List<Carta>();
             Monstruo carta1 = new Monstruo("mago oscuro", "el mas grande de los magos en cuanto ataque y defensa", false, 2, 3, 3, 3, EAtributo.oscuridad, ENivel.sieteEstrella, Etipo.lanzador_de_conjuros, 2500, 2000, false);
 
-    
+
 
             bool pudo = false;
 
             pudo = cartas + carta1;
 
-        
+
             Assert.IsTrue(pudo);
         }
 
@@ -35,19 +35,25 @@ namespace TestUnitarios
         /// analiza si guardo archivo 
         /// </summary>
         [TestMethod]
-        public void GuardeArchivo() 
+        public void GuardeArchivo()
         {
-            string ruta = AppDomain.CurrentDomain.BaseDirectory + "ListaCartasTest.xml"; //directorio + nombre archivo
+            string ruta = AppDomain.CurrentDomain.BaseDirectory + "ListaCartasTest1.xml"; //directorio + nombre archivo
 
 
-            List<Carta> cartas = new List<Carta>();
+            List<Carta> cartasTest1 = new List<Carta>
+            {  new Trampa("agujero trampa", "cuando tu adversario invoca de modo normal o por volteo un monstruo con 1000 atk o mas , selecciona ese monstruo y destruye ese objetivo", true, 10, 3, 3, 3, EtipoTrampa.normal),
+               new Magica("mostruo renacido", "invoca especialmente un monstruo desde cualquier cementerio", true, 10, 3, 3, 3, EtipoMagia.normal),
+               new Monstruo("Dragón Ciber Final", "Ciber Dragón + Ciber Dragón + Ciber DragónUna Invocación por Fusión de esta carta sólo puede hacerse con los Materiales de Fusión listados arriba. Si esta carta ataca a un monstruo en Posición de Defensa, inflige daño de batalla de penetración.", true, 10, 3, 3, 3, EAtributo.luz, ENivel.diezEstrella, Etipo.maquina, 4000, 2800, true)
+            };
 
+            List<Carta> cartasLeidas = new List<Carta>();
+            Xml<List<Carta>> xmlTest1 = new Xml<List<Carta>>(); //creo archivo xml
 
-            Xml<List<Carta>> xml = new Xml<List<Carta>>(); //creo archivo xml
+            xmlTest1.Guardar(ruta, cartasTest1);
+            xmlTest1.Leer(ruta, out cartasLeidas);
 
-
-            Assert.IsTrue(xml.Guardar(ruta, cartas));
-           
+           Assert.AreEqual(cartasTest1.Count, cartasLeidas.Count); // si hay la misma cantidadd de objetos guardados
+       
 
         }
 
